c068f6bee79f6d2fa2d74a6f777ea6cc8d1c9d21
kksahay <kunal.sahay4@gmail.com>
2018-02-28 18:15:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ function createTemplate(data){
                     <a href="/">Home</a>
                 </div>
                 <hr/>
-                <h3>${heading}}</h3>
+                <h3>${heading}</h3>
                 <div>
                     ${date}
                 </div>
