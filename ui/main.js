ff8b07a303ea4a51877c0b7ce68b7afd5aac1800
kksahay <kunal.sahay4@gmail.com>
2018-03-03 01:50:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,7 +21,7 @@ button.onclick=function(){
 };
 var nameInput=document.getElementById('name');
 var name=nameInput.value;
-var submit=getElementById('submit');
+var submit=getElementById('submitbtn');
 submit.onclick = function(){
     
     var names=['name1','name2','name3','name4'];
