be4bfb632a12cae517e4a0cd3575d04bd7d35232
kksahay <kunal.sahay4@gmail.com>
2018-03-03 01:42:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,16 @@ button.onclick=function(){
     request.send(null);
     
 };
+var nameInput=document.getElementById('name');
+var name=nameInput.value();
+var submit=getElementById('submit');
+submit.onclick = function(){
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length;i++){
+        list+= '<li>' + names[i] + '</li>';
+    }
+    var ul=document.getElementById('namelst');
+    ul.innerHTML=list;
+};
